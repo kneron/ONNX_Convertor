1. fix tf_pattern_match find name issue  2. do constant folding for OP:ConstOfShape  3. fix slice opset11 constant folding issue  4. fix reshape constant folding wrong dim issue

optimizer_scripts/tools/constant_folding.py
@@ -130,20 +130,32 @@ def slice_constant_folding_Opset_11(g, node):
     ends_node = helper.find_node_by_output_name(g, node.input[2])
     _, ends = helper.constant_to_list(ends_node)
 
-    axes_node = None if len(node.input) < 4 else helper.find_node_by_output_name(g, node.input[3])
+
+    axes_node = None if len(node.input) <= 3 else helper.find_node_by_output_name(g, node.input[3])
     if not axes_node:
         axes = list(range(len(helper.get_shape(data_list))))
     else:
         _, axes = helper.constant_to_list(axes_node)
 
+    steps_node = None if len(node.input) <= 4 else helper.find_node_by_output_name(g, node.input[4])
+    if not steps_node:
+        steps = [1]*len(helper.get_shape(data_list))
+    else:
+        _, steps = helper.constant_to_list(steps_node)
+
+
     data_list = list(map(int, data_list))
     starts = list(map(int, starts))
     ends = list(map(int, ends))
     axes = list(map(int, axes))
+    steps = list(map(int, steps))
 
     data_list = np.reshape(data_list, pre_shape)
 
-    new_data = helper.slice_data(data_list, starts, ends, axes)
+    new_data = None
+    for idx, _ in enumerate(axes):
+        new_data = np.apply_along_axis( lambda x: x[starts[idx] : ends[idx] : steps[idx]], idx, data_list )
+
     new_node = helper.list_to_constant(node.output[0], helper.get_shape(
         new_data), helper.flatten_to_list(new_data))
     g.node.extend([new_node])
@@ -288,7 +300,7 @@ def reshape_constant_input_folding(g, node):
     new_tensor = onnx.helper.make_tensor(
         name=node.output[0],
         data_type=pre_data_node.attribute[0].t.data_type,
-        dims=shape,
+        dims=new_data.shape,
         vals=helper.flatten_to_list(new_data)
     )
     new_node = onnx.helper.make_node(