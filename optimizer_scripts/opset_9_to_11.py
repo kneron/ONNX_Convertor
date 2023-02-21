66d3f831de2c8eb32c714ee2a69dd969d55a8c09
Jiyuan Liu <jiyuan@kneron.us>
2023-02-21 20:07:08 +0800

    Add new opset update scripts.

--- optimizer_scripts/opset_9_to_11.py
+++ optimizer_scripts/opset_9_to_11.py
@@ -147,13 +147,41 @@ def onnx1_4to1_6(model: onnx.ModelProto) -> onnx.ModelProto:
     Returns:
         onnx.ModelProto: updated onnx model.
     """
-    graph = model.graph
+    helper.logger.warning("This function is deprecated. Please use `convert_opset_9_to_11` instead.")
+    return convert_opset_9_to_11(model)
 
-    if model.opset_import[0].version >= 11:
-        helper.logger.error("(Stop) the input model opset is greater than 11, no need to upgrade")
-        exit(1)
+def convert_opset_9_to_11(model: onnx.ModelProto) -> onnx.ModelProto:
+    """Update opset from 9 to 11.
+
+    Args:
+        model (onnx.ModelProto): input onnx model.
+
+    Returns:
+        onnx.ModelProto: updated onnx model.
+    """
+    # Check opset
+    helper.logger.info("Checking and changing model meta data.")
+    if len(model.opset_import) == 0:
+        helper.logger.warning("Updating a model with no opset. Please make sure the model is using opset 9.")
+        opset = onnx.helper.make_opsetid('', 11)
+        model.opset.append(opset)
+    else:
+        for opset in model.opset_import:
+            if len(opset.domain) != 0:
+                continue
+            elif opset.version == 9:
+                opset.version = 11
+                break
+            elif opset.version >= 11:
+                helper.logger.error(f"Converting opset {opset.version} to opset 11. No need to convert.")
+                return None
+            else:
+                helper.logger.warn(f"Converting opset {opset.version} to opset 11. Only opset 9 is supported.")
+                opset.version = 11
+                break
 
     helper.logger.info("Adding required information for the model.")
+    graph = model.graph
     # deal with empty node name issue
     other.add_name_to_node(graph)
     # simplify the node param type from initializer to constant
@@ -167,24 +195,18 @@ def onnx1_4to1_6(model: onnx.ModelProto) -> onnx.ModelProto:
     upsampling_to_resize(graph)
     other.topological_sort(graph)
 
-    # Change model properties.
-    helper.logger.info("Changing model meta data.")
-    model.ir_version = 6
-    model.opset_import[0].version = 11
-
     while len(graph.value_info) > 0:
         graph.value_info.pop()
     model = onnx.utils.polish_model(model)
     return model
 
 if __name__ == "__main__":
-    print("***This script is deprecated. Please use `opset_9_to_11.py` instead.***")
     logging.basicConfig(level=logging.INFO)
     if len(sys.argv) != 3:
         print("Usage:{} file_in file_out".format(sys.argv[0]))
         exit(1)
 
     model = onnx.load(sys.argv[1])
-    model = onnx1_4to1_6(model)
+    model = convert_opset_9_to_11(model)
 
     onnx.save(model, sys.argv[2])
